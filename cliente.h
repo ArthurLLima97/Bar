b5c5955d09df3a5fafdd872873895e8025c3c708
RODRIGO NUNES CAMARGO <42442741+Rodrigocamarg0@users.noreply.github.com>
2018-11-23 19:20:17 -0200

    Create cliente.h

---
+++ cliente.h
@@ -0,0 +1,14 @@
+class cad {
+public:
+    string nome;
+		string cpf;
+		string telefone;
+		string card;
+	
+	cad(string nome, string cpf, string telefone, string card) {
+		this->nome = nome;
+		this->cpf = cpf;
+		this->telefone = telefone;
+		this->card = card;
+	}
+};
